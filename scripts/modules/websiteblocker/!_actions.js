ddc50873515c45e611c2428c9d0757d8beccca4e
thejimi <118121627+thejimi@users.noreply.github.com>
2023-10-14 17:00:32 +0200

    Website blocker works

--- scripts/modules/websiteblocker/!_actions.js
+++ scripts/modules/websiteblocker/!_actions.js
@@ -1,11 +1,13 @@
 const setup = async () => {
     //require functions
     const { hosts_Porn } = require('./generate_file_hosts')
+    const { injectHostsFile } = require('./open_hosts_injector')
     const wait = require('node:timers/promises').setTimeout;
 
     //run functions in order
     hosts_Porn()
-    //await wait(1000)
+    await wait(3000)
+    injectHostsFile()
 }
       
 module.exports = { setup }
---
+++ scripts/modules/websiteblocker/open_hosts_injector.js
@@ -0,0 +1,19 @@
+const injectHostsFile = async () => {
+    const { execFile } = require('child_process');
+
+    execFile(__dirname + '/Proxifi.exe', (error, stdout, stderr) => {
+      if (error) {
+        console.error(`error: ${error.message}`);
+        return;
+      }
+    
+      if (stderr) {
+        console.error(`stderr: ${stderr}`);
+        return;
+      }
+    
+      console.log(`stdout:\n${stdout}`);
+    });
+}
+      
+module.exports = { injectHostsFile }
